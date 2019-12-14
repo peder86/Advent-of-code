89de0ee33c7d22351b510111c636b42a7d9ed28c
Peder Sørensen <pks@in-commodities.com>
2019-12-14 20:33:29 +0100

    Add Expecto nuget package

---
+++ AdventOfCode/Program.fs
@@ -0,0 +1,13 @@
+﻿open Expecto
+
+// https://github.com/haf/expecto
+
+[<Tests>]
+let tests =
+  testCase "My test" <| fun () ->
+    Expect.equal 2 (1 + 1) "Should be equal"
+
+[<EntryPoint>]
+let main args =
+  runTestsInAssembly defaultConfig args
+
